3dd8247705134eebde10791d2106eada3311c4b9
steve.brazier <meadsteve@gmail.com>
2015-11-11 21:58:58 +0000

    add supervisor to proccess to kill

--- lib/chaos_spawn_example.ex
+++ lib/chaos_spawn_example.ex
@@ -5,11 +5,12 @@ defmodule ChaosSpawnExample do
   # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+    alias ChaosSpawn.Chaotic.ChaoticSupervisor
 
     children = [
       # Start the endpoint when the application starts
       supervisor(ChaosSpawnExample.Endpoint, []),
-      supervisor(ChaosSpawnExample.Supervisor, [])
+      ChaoticSupervisor.supervisor(ChaosSpawnExample.Supervisor, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
